886f3051080a87c7dfb1df7b1d3d006bd6280797
Syed Ahamed Ali <ahamedalisyed10@gmail.com>
2025-11-05 13:28:35 +0000

    Fixed CSS path for Streamlit Cloud

--- sentiment_analysis_app_v1/app/app.py
+++ sentiment_analysis_app_v1/app/app.py
@@ -30,7 +30,9 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css(os.path.join("app", "static", "style.css"))
+css_path = Path(__file__).parent / "static" / "style.css"
+local_css(css_path)
+
 
 # -------------------------------
 # 🌌 Header
